b944e8e3328de9067bba6767ff66bc0b77c9bdd7
Andrew Chaiko <av.chaiko@gmail.com>
2024-10-21 19:37:49 +0300

    WIP

--- Components/MoveDirectionComponent.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace Core.ECS.Components
-{
-    public class MoveDirectionComponent
-    {
-        public Vector3 Direction;
-    }
-}
--- Runtime/Components/MovingComponent.cs
+++ Runtime/Components/MovingComponent.cs
@@ -1,9 +1,11 @@
 using Scellecs.Morpeh;
+using UnityEngine;
 
 namespace Core.ECS.Components
 {
     public struct MovingComponent : IComponent
     {
+        public Vector3 Direction;
         public float StartTime;
         public float Duration;
     }
